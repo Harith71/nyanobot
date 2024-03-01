9de9528e240530d26c9c4f50cac30ab820ae270b
Harith71 <83798033+Harith71@users.noreply.github.com>
2024-02-29 19:15:39 -0600

    Update attr.js

--- src/commands/attr.js
+++ src/commands/attr.js
@@ -1,138 +1,121 @@
+// Import necessary modules
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const config = require('../config/config.json');
-const emoji = require('../config/emojis.json')
-const { downloadAndSaveImage, readCSV, setClient, getEmojiForTrait } = require('../utils.js')
-const { fetchAssetData, fetchUserProfileInfo, client } = require('../nft_bot.js')
-const { consoleError, consoleLog } = require('../debug.js');
-const fetch = require('isomorphic-fetch'); // Require the 'node-fetch' library
+const emoji = require('../config/emojis.json');
+const { downloadAndSaveImage, readCSV, setClient, convertToPercentage, getEmojiForTrait } = require('../utils.js');
+const { consoleLog, consoleError } = require('../debug.js');
+const { fetchAssetData, client } = require('../nft_bot.js');
+
+
+// Extract configuration values
 const prefix = config.prefix;
-// const client = new Client({
-//     intents: [
-//         GatewayIntentBits.Guilds,
-//         GatewayIntentBits.GuildMessages,
-//         GatewayIntentBits.MessageContent,
-//         GatewayIntentBits.GuildMessageReactions
-//     ],
-// });              
-
-async function shortenURL(fullURL) {
-    try {
-        // Fetch the TinyURL shortening API
-        const response = await fetch(`http://tinyurl.com/api-create.php?url=${encodeURIComponent(fullURL)}`);
-        if (response.ok) {
-            const shortURL = await response.text();
-            return shortURL;
-        } else {
-            throw new Error(`Failed to shorten URL: ${response.statusText}`);
-        }
-    } catch (error) {
-        consoleError('Error shortening URL:', error);
-        return fullURL; // Return the full URL in case of an error
-    }
-}
-
 const fileData = {};
 const imageData = {};
 
-// Export the module
+// Export the command module
 module.exports = {
     name: 'attr',
-    description: 'Retrieve the attribute count for a user.',
-    setClient,     
-    // Execute function to handle 'link' command
+    description: 'Retrieve attr of the cat',
+    setClient,
+
+    // Execute function for handling the 'info' command
     async execute(message) {
-        // Ignore messages from bots
         if (message.author.bot) return;
-        // Check if the message starts with the 'link' command
+
         if (message.content.startsWith(prefix + 'attr')) {
-            // Read CSV files into memory
+            // Read CSV files into fileData and imageData objects
             await readCSV(fileData, imageData);
+
             const args = message.content.slice(prefix.length).trim().split(/ +/);
             const command = args.shift().toLowerCase();
-            // Check if the command is 'link'
-            if (command === 'attr') {
-                const argument = args[0];
-                if (isNaN(argument)) {
-                    // Check if the argument is a number (fileNumber) or a username
-                    const username = argument;
-                    try {
-                        const userData = await fetchUserProfileInfo(username);
-                        const fields = [];
-                        for (const category in userData.attributes) {
-                            consoleLog(category);
-                            const subAttributes = userData.attributes[category];
-                            const emojiForTrait = getEmojiForTrait(category);
-                            consoleLog(emojiForTrait);
-                        
-                            const categoryLink = encodeURIComponent(category);
-                        
-                            const categoryField = {
-                                name: `\`${emojiForTrait}\` __**${category}:**__`,
-                                value: (await Promise.all(
-                                    Object.entries(subAttributes)
-                                        .filter(([subAttr, count]) => count > 0)
-                                        .map(async ([subAttr, count]) => {
-                                            try {
-                                                const encodedSubAttr = encodeURIComponent(subAttr);
-                                                const encodedCategorySubAttr = encodeURIComponent(`${category}:${subAttr}`);
-                                                const fullURL = `https://nanswap.com/art/collection/Nyano-Cats?attributesFilter=%5B%7B"label"%3A"${encodedSubAttr}"%2C"value"%3A"${encodedCategorySubAttr}"%7D%5D`;
-                            
-                                                // Shorten the URL asynchronously
-                                                const shortURL = await shortenURL(fullURL);
-                                                consoleLog(shortURL)
-                                                consoleLog(subAttr)
-                                                consoleLog(count)
-                                            
-                            
-                                                return `**[${subAttr}](${shortURL}):** \`${count}\``;
-                                            } catch (error) {
-                                                consoleError(`Error encoding/shortening attribute (${category}:${subAttr}):`, error);
-                                                return `- Error encoding/shortening attribute: ${subAttr}`;
-                                            }
-                                        })
-                                )).join(' | ') || 'None',
-                                inline: false,
-                            };
-                        
-                            fields.push(categoryField);
-                        }
-                        
 
-                        
-                        const userLink = `https://nanswap.com/art/${username}${config.referral}`;
-                        const imageUrl = userData.avatar || 'https://toolset.com/wp-content/uploads/2018/06/909657-profile_pic.png';
-                        const imageName = `${userData.id.replace(' ', '').replace('#', '-')}.png`;
+            if (command === 'attr') {
+                const fileNumber = parseInt(args[0]);
+
+                if (fileNumber in fileData && fileNumber in imageData) {
+                    try {
+                        // Fetch asset data using the asset ID from imageData
+                        const assetData = await fetchAssetData(imageData[fileNumber].id);
+
+                        const imageUrl = assetData.asset.location;
+                        const imageName = `${assetData.asset.id.replace(' ', '').replace('#', '-')}.png`;
+
+                        // Download and save the image locally
                         await downloadAndSaveImage(imageUrl, imageName);
-                        const embed = new EmbedBuilder()
+
+                        const assetURL = `https://nanswap.com/art/assets/${assetData.asset.id}${config.referral}`;
+                        
+                        const fields = []
+
+
+                        fields.push(
+                            {
+                                name: `\n\u200b`,
+                                value: `${emoji.Attributes} __**Attributes:**__`,
+                                inline: false
+                            },
+                        );
+
+                         // Iterate through metadata attributes and add them to the fields array
+                         for (const trait of assetData.asset.metadata.attributes) {
+                            const traitType = trait.trait_type;
+                            const traitValue = trait.value;
+
+                            if (assetData.asset.collectionId.attributes.hasOwnProperty(traitType)) {
+                                const subAttributes = assetData.asset.collectionId.attributes[traitType];
+
+                                if (subAttributes.hasOwnProperty(traitValue)) {
+                                    const subAttributeValue = subAttributes[traitValue];
+                                    const percentageValue = convertToPercentage(subAttributeValue);
+
+                                    const emojiForTrait = getEmojiForTrait(traitType);
+                                    fields.push({
+                                        name: `\`${emojiForTrait}\` **${traitType}:**`,
+                                        value: `\`\`\`${convertToPercentage(traitValue)} ${percentageValue}\`\`\``,
+                                        inline: true,
+                                    });
+                                } else {
+                                    const emojiForTrait = getEmojiForTrait(traitType);
+                                    fields.push({
+                                        name: `\`${emojiForTrait}\` **${traitType}:**`,
+                                        value: `\`\`\`${convertToPercentage(traitValue)}\`\`\``,
+                                        inline: true,
+                                    });
+                                }
+                            } else {
+                                const emojiForTrait = getEmojiForTrait(traitType);
+                                fields.push({
+                                    name: `\`${emojiForTrait}\` **${traitType}:**`,
+                                    value: `\`\`\`${convertToPercentage(traitValue)}\`\`\``,
+                                    inline: true,
+                                });
+                            }
+                        }
+
+                        // Create an Embed object with the specified details
+                        const Embed = new EmbedBuilder()
                             .setColor(0x0099FF)
-                            .setTitle(`${emoji.Lock} Attribute List Request for ${username}!`)
-                            .setDescription(`**Here is a list of ${username}'s owned attributes!**`)
+                            .setTitle(`${emoji.Project} ${imageData[fileNumber].name}`)
+                            .setThumbnail(`attachment://${imageData[fileNumber].id}.png`)
                             .addFields(fields)
-                            .setURL(userLink)
-                            .setThumbnail(`attachment://${userData.id}.png`)
-                            .setFooter({ text: 'Nyano Bot | Powered by Armour', iconURL: config.embedFooterImage })
+                            .setURL(assetURL)
+                            .setFooter({ text: 'Nyano Cats Bot', iconURL: config.embedFooterImage })
                             .setTimestamp();
+
+                        // Send a message with the Embed and attached image
                         message.reply({
-                            content: `${message.author}, here is the requested Attribute Count for ${username}.`,
-                            embeds: [embed],
-                            files: [{ attachment: imageName, name: `${userData.id}.png` }],
+                            content: `${message.author} has requested attribute information on ${imageData[fileNumber].name}.`,
+                            embeds: [Embed],
+                            files: [{ attachment: imageName, name: `${imageData[fileNumber].id}.png` }],
                         });
                     } catch (error) {
-                        consoleError('Error processing username request:', error);
-                        message.reply(`${message.author}, an error occurred while processing the request. Please check if the username is correct and try again.`);
+                        consoleError('Error fetching or processing asset data:', error);
+                        message.reply(`${message.author}, an error occurred while processing the request.`);
                     }
                 } else {
-                    consoleLog("wrong args")
+                    message.reply(`${message.author}, file not found. Please provide a valid file number.`);
                 }
-            } else {
-                consoleLog("not in dm");
             }
-        } else {
-            consoleLog("no perms or invalid command");
         }
-    }
+    },
 };
-
-
-// Login to the Discord client using the provided token
-// client.login(config.token);
